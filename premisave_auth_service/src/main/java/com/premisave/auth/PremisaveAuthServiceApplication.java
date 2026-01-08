4220ea23da6af7ed3253bbd9026a36a1dd79224f
Bill Graham Peacemaker <grahambill011@gmail.com>
2026-01-09 00:30:13 +0300

    Creating a new auth backend microservice

---
+++ premisave_auth_service/src/main/java/com/premisave/auth/PremisaveAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.premisave.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PremisaveAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PremisaveAuthServiceApplication.class, args);
+	}
+
+}
